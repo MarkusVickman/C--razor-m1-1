843f50cdc1b0ae89bfecfeb851cde1ae4630928a
Markus <markus.vickman@hotmail.com>
2025-01-22 08:26:21 +0100

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const menuButton = document.getElementById("menu-open-btn");
+const menuButtonClose = document.getElementById("menu-close");
+const mainNav = document.getElementById("navbar");
+
+menuButton.addEventListener("click", function (e){
+    mainNav.style.display = "block";
+    menuButton.style = "display:none";
+    menuButtonClose.style = "display:block";
+})
+
+menuButtonClose.addEventListener("click", function (e){
+    mainNav.style.display = "none";
+    menuButton.style = "display:block";
+    menuButtonClose.style = "display:none";
+})
+
+//Funktion som tar bort styling i html dokumentet för att menyn ska visas korrekt på datorer.
+function resize() {
+    if (window.innerWidth > 600) {
+        mainNav.style.display = "block";
+        menuButton.style.display = "none";
+        menuButtonClose.style.display = "none";
+    } else {
+        mainNav.style.display = "none";
+        menuButton.style.display = "block";
+    }
+}
+
+//initierar funktion vid ändrad visningstorlek
+window.onresize = resize;
