f13a05edb4048f141d116a4a7bc96036d407212e
Markus <markus.vickman@hotmail.com>
2025-01-22 12:22:40 +0100

    publicerat

--- obj/Debug/net9.0/moment1-1.AssemblyInfo.cs
+++ obj/Debug/net9.0/moment1-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moment1-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843f50cdc1b0ae89bfecfeb851cde1ae4630928a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b46c1a41255a4da0eee5e30505ed9b065456e601")]
 [assembly: System.Reflection.AssemblyProductAttribute("moment1-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moment1-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
